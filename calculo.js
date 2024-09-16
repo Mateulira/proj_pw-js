2dde33f05e0a20fdea266275a36d7bee9bfd550c
Mateus Lira <mateus.lopes39@etec.sp.gov.br>
2024-09-16 17:44:11 -0300

    introdução ao javascript

--- calculo.js
+++
@@ -1,8 +0,0 @@
-function somar() {
-    var x = 2
-    var y = 2
-    let res = x + y
-    document.getElementById("soma").innerHTML=res
-    document.getElementById("conf-soma").innerHTML="Calculado✔"
-    
-}
